56fb3c9d5cc665ab10fcee37045aa39ec090ff1e
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-14 14:37:51 +0100

    word control

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* chiedere all'utente una parola quindi creare una funzione per capire
+se la parola inserita è palindroma */ 
+
+// chidere all'utente una parola
+
+do {
+    
+    uParola = prompt('dimmi una parola').trim();
+    
+    console.log(uParola);
+
+} while ((uParola  <= 0) || (!isNaN(uParola))) {
+
+
+}
+
+
