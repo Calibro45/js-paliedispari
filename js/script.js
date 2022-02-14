949a8a5e07fea21021959e815793f505e80391e5
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-14 15:03:04 +0100

    parola al contrario

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ se la parola inserita è palindroma */
 
 do {
     
-    uParola = prompt('dimmi una parola').trim();
+    uParola = prompt('dimmi una parola').trim().toLocaleLowerCase();
     
     console.log(uParola);
 
@@ -14,4 +14,26 @@ do {
 
 }
 
+const arrayParola = [];
+
+const parolaSplit = uParola.split('');
+console.log(parolaSplit);
+
+arrayParola.push(uParola);
+
+const parolaReverse = parolaSplit.reverse();
+
+console.log(parolaReverse);
+
+const parolaReverseFinale = parolaReverse.join('');
+console.log(parolaReverseFinale);
+
+// creare funzione 
+
+function palindromo (parola1) {
+    
+
+    
+    
+}
 
